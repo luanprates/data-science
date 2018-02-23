a71a286c60d860de2fc4d661a8ddb18b44533830
luanprates <32576124+luanprates@users.noreply.github.com>
2018-02-22 22:07:14 -0300

    deixando o código mais limpo

--- R/Crimes em Fortaleza.R
+++ R/Crimes em Fortaleza.R
@@ -8,29 +8,17 @@ library(downloader)
 
 # Configurando o diretório de trabalho
 # Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-setwd("~\GitHub\data-science\R")
+setwd("~/gitHub/data-science/R")
 getwd()
 
 # Criando o dataframe
 arquivo <- "http://dados.fortaleza.ce.gov.br/dataset/8e995f96-423c-41f3-ba33-9ffe94aec2a8/resource/de4e876a-ee24-4d6e-9722-db9dc454bbe6/download/policecalls.csv"
 df1 <- read.csv(arquivo)
-head(df1)
-str(df1)
-dim(df1)
 
 # Criando o plot
 options(warn=-1)
  mapa <- qmap("fortaleza", zoom = 12, source = "stamen", 
               maptype = "toner", darken = c(.3,"#BBBBBB"))
-
-mapa
-
-# Mapeando os dados 
-mapa + geom_point(data = df1, aes(x = lng, y = lat))
-
-# Mapeando os dados e ajustando a escala
-mapa + geom_point(data = df1, aes(x = lng, y = lat), 
-                  color = "dark green", alpha = .03, size = 1.1)
 
 # Mapeando os dados e definindo uma camada de intensidade
 mapa +
